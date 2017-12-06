0a9d98cb768d39f5888b55d965f9918c77d31770
Franck Martin <fmartin40@gmail.com>
2017-12-06 14:49:40 +0100

    upload videoApp files

---
+++ videoApp/src/components/search_bar.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+
+class SearchBar extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {term: ''};
+  }
+
+  render() {
+    return (
+      <div className="search-bar">
+        <input
+          value={this.state.term}
+          onChange={event => this.onChangeinput(event.target.value)}/>
+      </div>
+    )
+  }
+
+  onChangeinput(term) {
+    this.setState({term});
+    this.props.onSearchTermChange(term);
+  }
+}
+
+export default SearchBar;
